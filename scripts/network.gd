7f41e224beeace3a6d70197db699d252cf3b56dc
Oliver Gilliland <omgilliland@gmail.com>
2022-05-11 19:58:54 +0100

    Networked character controller

--- scripts/network.gd
+++ scripts/network.gd
@@ -8,6 +8,10 @@ var client: NetworkedMultiplayerENet
 
 var ip_address: String = ""
 
+# Create placeholder reference to game scene which we populate on ready in the game script
+# This is a bit hacky but it works for now ¯\_(ツ)_/¯
+var game: Spatial
+
 func _ready() -> void:
 	for ip in IP.get_local_addresses():
 		if ip.begins_with("192.168."):
@@ -26,6 +30,10 @@ func create_server() -> void:
 	server = NetworkedMultiplayerENet.new()
 	server.create_server(DEFAULT_PORT, MAX_CLIENTS)
 	get_tree().set_network_peer(server)
+	
+	# Use the reference to the game scene populated earlier
+	# Instance a player for the server host on their own machine
+	game.instance_player(get_tree().get_network_unique_id())
 
 func join_server() -> void:
 	client = NetworkedMultiplayerENet.new()
