7c9d697d14c5c1d2c68f5f8165ca239edbb2ee0e
Oliver Gilliland <omgilliland@gmail.com>
2022-05-12 18:48:46 +0100

    Add dynamic UI scaling

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -8,8 +8,24 @@ onready var player_name_input = $MainMenu/PlayerName
 onready var game = $GameViewport/Viewport/Game
 onready var lobby = $Lobby
 
+func _resize() -> void:
+	var size = get_viewport().size
+	var scale = size.x / 1024 - 0.5
+	scale = clamp(scale, 0.5, 4.0)
+	var nodes_to_resize = [
+		$GameViewport/HUD,
+		$Lobby,
+		$MainMenu
+	]
+	for node in nodes_to_resize:
+		node.anchor_right = 1.0/scale
+		node.anchor_bottom = 1.0/scale
+		node.rect_scale = Vector2(scale, scale)
 
 func _ready() -> void:
+	get_tree().get_root().connect("size_changed", self, "_resize")
+	_resize()
+	
 	get_tree().connect("network_peer_connected", self, "_player_connected")
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 	get_tree().connect("connected_to_server", self, "_server_connected")
