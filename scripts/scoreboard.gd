3375b4b5a0e35d905d61bd73434a55b3d044cadf
Branden Soropia <branden@0300CAHF.local>
2022-05-12 13:08:07 -0400

    Add prototype scoreboard UI with test button.

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -1,19 +1,21 @@
-extends Node
+extends Label
 
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-var score = 0
+
 
 sync func _add_score(val: int) -> void:
 	score += val
+	text = "Score: %s" % score
 	print(score)
 
-func add_score(item: Node) -> void:
-	var score = 10
-	
+func add_score() -> void:
+	var value = 10
+	_add_score(value)
 	# TODO calculate score of a box
 	
 	rpc("_add_score", score)
@@ -21,3 +23,8 @@ func add_score(item: Node) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+# Just for testing the scoreboard UI! Toggle TestAddPoints to visible!
+func _on_TestAddPoints_pressed():
+	add_score()
+	pass # Replace with function body.
