263bfcb7f205dc6acc7ae71e23341690a0f6c948
Oliver Gilliland <omgilliland@gmail.com>
2022-05-13 05:38:39 +0100

    Add score to UI

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -1,6 +1,8 @@
-extends Label
+extends Node
 
 var score = 0
+
+var label: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,21 +12,13 @@ func _ready():
 
 sync func _add_score(val: int) -> void:
 	score += val
-	text = "Score: %s" % score
-	print(score)
+#	text = "Score: %s" % score
+	label.text = str(score)
+	print_debug(score)
 
 func add_score() -> void:
 	var value = 10
-	_add_score(value)
 	# TODO calculate score of a box
 	
 	rpc("_add_score", score)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-# Just for testing the scoreboard UI! Toggle TestAddPoints to visible!
-func _on_TestAddPoints_pressed():
-	add_score()
-	pass # Replace with function body.
